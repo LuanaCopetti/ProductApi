bf2919fd82753692ea7703ed90391d5e0a6bbeb1
Luana Copetti <86331342+LuanaCopetti@users.noreply.github.com>
2023-08-23 10:38:33 -0300

    commit

---
+++ ProductApi/NovaPasta/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductApi.NovaPasta
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public decimal Preco { get; set; }
+        public int Quantidade { get; set; }
+    }
+
+}
